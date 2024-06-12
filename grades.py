929bfa942e3ce841911cc6c1befd59776a7a56fa
first <baarya24@tbc.edu.np>
2024-06-12 21:32:06 +0545

    graders

--- grades.py
+++ grades.py
@@ -1,3 +1,21 @@
+import csv
+
 class grades:
-    __init__(self, id, maths, fod, fom, english, it):
+
+    def list_grades(self):
+        with open("grades.csv", mode = r) as file:
+            grades = csv.reader(file)
+            print("|    Student ID  |    FOD    |    FOM   |   Maths   |   English |   IT  |")
+            for details in grades:
+                print(f"|   {details[0]}  |   {details[1]}  |   {details[2]}  |   {details[3]}  |   {details[4]}  |   {details[5]}  |")
     
+    def grade_adder(self):
+        with open("grader.csv", mode = a) as file:
+            adder = csv.writer(file)
+            id = print("Enter the user ID: ")
+            fod = print("Enter FOD Grades: ")
+            fom = print("Enter FOM Grades: ")
+            maths = print("Enter Maths Grades: ")
+            english = print("Enter English Grades: ")
+            it = print("Enter IT Grades: ")
+            adder.writerow([id,fod,fom,maths,english,it])
